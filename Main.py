eb371530a8fdd74d5d68e16fcd29c9578d585fd2
Joshua Fields <JoshuaFieldsus@Gmail.com>
2025-03-13 23:41:28 +0000

    Updated email authentication to use App Password

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
 app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS", "True") == "True"
 app.config['MAIL_USE_SSL'] = os.getenv("MAIL_USE_SSL", "False") == "True"
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME", "joshuafields.dev@gmail.com")
-app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD", "")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD", "ozau zvjd tgmt gxnc")
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 
 mail = Mail(app)
